9224c5be09587196e388b8c19833a2e3a1805bb0
Mani Tyagi <taggymani297@gmail.com>
2023-12-24 19:28:11 -0500

    solving Q2

---
+++ 2023/Q2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <omp.h>
+#include <random>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+pair<double,double> caluclate_area(double inner_radius, double outer radius, int num_rand_samples){
+       
+}	
+
+int main() {
+   
+   double r = 10.0;
+   double R = 20.0;
+   int samples[] = {8, 16, 32, 64, 128, 256};
+      
+   return 0;
+}
+
+	
