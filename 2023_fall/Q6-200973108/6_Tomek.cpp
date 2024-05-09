09d277042c71c0d305e43fc0391ad2135802109c
Mani0237 <taggymani297@gmail.com>
2024-05-08 20:23:30 -0500

    tomek's question started

---
+++ 2023_fall/Q6-200973108/6_Tomek.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+// function to initialize the domain with initial conditions
+void initialize_domain(){
+
+}
+
+// function to apply boundary conditions
+void apply_boundary_conditions(){
+
+}
+
+// function to evaluate source term
+double evaluate_source_term(double x){
+
+}
+
+// function to solve diffusion equation using finite differences
+void solve_diffusion(){
+    
+}
+
+int main() {
+
+    return 0;
+}
