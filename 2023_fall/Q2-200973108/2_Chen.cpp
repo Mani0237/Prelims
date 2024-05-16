557fdf45aba21e3d577738e7381be76475270b7d
Mani0237 <taggymani297@gmail.com>
2024-05-16 14:01:21 -0500

    done

--- 2023_fall/Q2-200973108/2_Chen.cpp
+++ 2023_fall/Q2-200973108/2_Chen.cpp
@@ -54,6 +54,7 @@ double accept_reject_monte_carlo(const Point* centers, const double* radius, int
 }
 
 int main() {
+    
     Point centers[] = { { -2, 0 }, { 2, 0 }, { 0, 2 } };
     double radius[] = { 3, 3, 3 };
     int num_samples_list[] = { 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000 };
