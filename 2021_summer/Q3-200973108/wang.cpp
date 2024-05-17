aa3386871e906711fcb65facdbb8091d1048527e
Mani0237 <taggymani297@gmail.com>
2024-05-16 22:37:00 -0400

    wang opemmp

---
+++ 2021_summer/Q3-200973108/wang.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+//#include <omp.h>
+#define n 1000000000
+
+using namespace std;
+unsigned char s[n];
+
+int main() {
+    int i;
+    for(i=0; i<n; i++) {
+        s[i] = i*i % 256;
+    }
+
+    int num[256] = {0};
+    for (int i=0; i<n; i++) {
+        num[s[i]]++;
+    }
+
+    for (int i=0; i<256; i++) { 
+        printf("The char %d freq is %d\n", i, num[i]);
+    }
+}
