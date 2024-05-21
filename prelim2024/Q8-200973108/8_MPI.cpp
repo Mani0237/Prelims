95ef1cc45f29dbb58467186c302cd0386a8e0e93
Mani0237 <taggymani297@gmail.com>
2024-05-21 16:29:45 -0500

    olmo fixed

--- prelim2024/Q8-200973108/8_MPI.cpp
+++ prelim2024/Q8-200973108/8_MPI.cpp
@@ -6,17 +6,21 @@
 
 using namespace std;
 
+// Function to be integrated
 double h(double x) {
     return exp(x);
 }
 
+// Simpson's rule to be used by MPI processes
 double integrate_simpsons(double x0, double x2, double dx) {
     return (dx / 3.0) * (h(x0) + 4 * h(x0 + dx / 2.0) + h(x2));
 }
 
 int main(int argc, char** argv) {
+    // Initialize MPI
     MPI_Init(&argc, &argv);
 
+    // Get rank and size of the MPI processes
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -24,8 +28,9 @@ int main(int argc, char** argv) {
     const int Np = size; // Number of processes
     const int N = 10;    // Number of subintervals per process
 
-    double a = 0.0;
-    double b = 1.0;
+    double a = 0.0;    // Lower limit of integration
+    double b = 1.0;    // Upper limit of integration
+
     double dx = (b - a) / (2 * Np * N); // Width of each subinterval
 
     double local_sum = 0.0;
@@ -38,26 +43,34 @@ int main(int argc, char** argv) {
         x0 += 2 * dx; // Move to the next subinterval
     }
 
+    // exchanging function values
+    if (rank < size - 1) {
+        double next_h2 = h(x2 + dx);
+        MPI_Send(&next_h2, 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
+    }
+    if (rank > 0) {
+        MPI_Recv(&x2, 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
     // Gather all local results on process 0
     vector<double> all_local_sums(size);
     MPI_Gather(&local_sum, 1, MPI_DOUBLE, all_local_sums.data(), 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    // Output results on process 0
+    // Output results
     if (rank == 0) {
         cout << "-------------------------" << endl;
         cout << setw(10) << left << "Process" << setw(15) << "Area" << endl;
         cout << "-------------------------" << endl;
+        double total_sum = 0.0;
         for (int i = 0; i < size; ++i) {
             cout << setw(10) << left << i << setw(15) << all_local_sums[i] << endl;
+            total_sum += all_local_sums[i];
         }
         cout << "-------------------------" << endl;
-        double total_sum = 0.0;
-        for (double local_sum : all_local_sums) {
-            total_sum += local_sum;
-        }
         cout << "Full approximation: " << total_sum << endl;
     }
 
+    // Finalize MPI
     MPI_Finalize();
     return 0;
 }
--- prelim2024/Q8-200973108/8_openmp.cpp
+++ prelim2024/Q8-200973108/8_openmp.cpp
@@ -9,6 +9,8 @@
 using namespace std;
 using namespace std::chrono;
 
+
+// Function to be integrated
 double h(double x) {
     return exp(x);
 }
@@ -16,12 +18,13 @@ double h(double x) {
 
 int main() {
 
-    const int n = 1000000; 
-    double a = 0.0;
-    double b = 1.0;
-    const double I_exact = 1.718281828459045;
-    vector<int> thread_counts = {1, 2, 4, 6, 8, 10, 16, 28}; 
+    const int n = 1000;    // Number of intervals
+    double a = 0.0;        // lower limit of integration
+    double b = 1.0;        // upper limit of integration
+    const double I_exact = 1.718281828459045;                  // exact value from integral calculator
+    vector<int> thread_counts = {1, 2, 4, 6, 8, 10, 16, 28};   // thread count
 
+    // formating the output
     cout << "------------------------------------------------------------" << endl;
     cout << setw(10) << left << "Threads"
          << setw(10) << left << "I"
@@ -30,12 +33,23 @@ int main() {
          << endl;
     cout << "------------------------------------------------------------" << endl;
 
+    // Loop over different threads
     for (int num_threads : thread_counts) {
+
+        // starting the timer
         auto start_time = high_resolution_clock::now();
+
+        // caling function to calculate integral
         double I = simpson(n, a, b, num_threads);
+        
+        // stoping the timer
         auto end_time = high_resolution_clock::now();
         double elapsed_time = duration_cast<microseconds>(end_time - start_time).count();
+        
+        // calculting error
         double error = abs(I - I_exact);
+        
+        // printing the results for each thread
         cout << setw(10) << num_threads 
              << setw(10) << I 
              << setw(18) << error 
